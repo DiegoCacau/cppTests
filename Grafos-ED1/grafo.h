b7bd25d11ffecc0b9db01e44de6d048859a39e0a
Diego <dc_cacau@hotmail.com>
2017-06-19 11:21:01 -0300

    programa de grafos finalizado

--- Grafos-ED1/grafo.h
+++ Grafos-ED1/grafo.h
@@ -17,34 +17,45 @@ typedef struct grafo{
     TNO *prim_no;
 }TG;
 
-// componente conexos
+// componente usados na buscaP
 static int *pre,*low;
-static TNO **stack;
-static int cnt;
+static TNO **pilha;
+static int cont;
 static int k, N;
 
 // Funcoes para criaçao do grafo
 
 TG *cria(void){
+
     TG *g = (TG*)malloc(sizeof(TG));
     g->prim_no = NULL;
     return g;
 }
 
+//Retorna no, caso exista
+
 TNO *busca_no(TG *g, int no){
+
     TNO *p = g->prim_no;
+
     while((p)&&(p->id_no != no)){
         p = p->prox_no;
     }
+
     return p;
 }
 
+//Insere no
+
 void insere_no(TG *g, int no){
+
     TNO *p = busca_no(g, no);
+
     if(p) {
         printf("Esse no ja existe\n");
         return;
     }
+
     p = (TNO*)malloc(sizeof(TNO));
     p->id_no = no;
     p->prim_viz = NULL;
@@ -52,39 +63,56 @@ void insere_no(TG *g, int no){
     g->prim_no = p;
 }
 
+// Retorna aresta, caso ela exista
+
 TViz* busca_aresta(TG *g, int no1, int no2){
+
     TNO* viz1 = busca_no(g, no1);
+
     if(!viz1){
         return NULL;
     }
+
     TNO *viz2 = busca_no(g, no2);
+
     if(!viz2){
         return NULL;
     }
+
     TViz *p = viz1->prim_viz;
+
     while((p)&&(p->id_viz!=no2)){
         p=p->prox;
     }
+
     return p;
 }
 
+// Insere vizinho ao no
+
 void insere_viz(TG* g, int no, int id_viz){
+
     TNO *p = busca_no(g, no);
+
     if(!p){
         return;
     }
+
     TNO *l = busca_no(g, id_viz);
+
     if(!l){
         return;
     }
-    
+
     TViz *v = p->prim_viz;
+
     while(v){
         if(v->id_viz == id_viz){
             return;
         }
         v = v->prox;
     }
+
     v = (TViz*)malloc(sizeof(TViz));
     v->id_viz = id_viz;
     v->prox = p->prim_viz;
@@ -94,14 +122,19 @@ void insere_viz(TG* g, int no, int id_viz){
 // Print
 
 void imprime(TG* g){
+
     TNO *p = g->prim_no;
+
     while(p){
+
         printf("%d",p->id_no);
         TViz *viz = p->prim_viz;
+
         while(viz){
             printf("-> %d", viz->id_viz);
             viz = viz->prox;
         }
+
         p = p->prox_no;
         printf("\n");
     }
@@ -110,6 +143,7 @@ void imprime(TG* g){
 // Free
 
 void liberaViz(TViz *q){
+
     TViz *temp = q;
     while(q){
         temp = q;
@@ -117,63 +151,110 @@ void liberaViz(TViz *q){
         free(temp);
     }
     free(q);
+
 }
 
 void libera(TNO* no){
+
     TViz *q = no->prim_viz;
     liberaViz(q);
     free(no);
+
+}
+
+void liberaGrafo(TG* g){
+
+    TNO *n = g->prim_no;
+
+    while(n){
+        TNO *m = n;
+        n = n->prox_no;
+    
+        libera(m);
+    }
+
+    free(g); 
 }
 
 // Funcoes para retirar um no do grafo e todas as ligacoes que continham esse id
 
 TViz *retirador_de_viz(TViz* l, int no){
-    if(!l) return l;
+
+    if(!l) 
+        return l;
+
     TViz *p = l, *ant = NULL;
+
     if(l->id_viz == no){
         l = l->prox;
         return l;
     }
+
     while((p)&&(p->id_viz!=no)){
         ant = p;
         p = p->prox;
     }
-    if(!p) return l;
-    if(!ant) l = l->prox;
-    else ant->prox = p->prox;
+
+    if(!p) 
+        return l;
+
+    if(!ant) 
+        l = l->prox;
+    else 
+        ant->prox = p->prox;
+
     free(p);
     return l;
+
 }
 
 TNO *retirador_de_nos(TNO* l, int no){
-    if(!l) return l;
+
+    if(!l) 
+        return l;
+
     TNO *p = l, *ant = NULL;
+
     if(l->id_no == no){
         l = l->prox_no;
         return l;
     }
+
     while((p)&&(p->id_no!=no)){
         ant = p;
         p = p->prox_no;
     }
-    if(!p) return l;
-    if(!ant) l = l->prox_no;
-    else ant->prox_no = p->prox_no;
+
+    if(!p) 
+        return l;
+
+    if(!ant) 
+        l = l->prox_no;
+    else 
+        ant->prox_no = p->prox_no;
+
     free(p);
     return l;
+
 }
 
+// Retira um vertice especifico
+
 void retira(TG* g, int x){
+
     TNO* no =  g->prim_no;
     g->prim_no = retirador_de_nos(no, x);
+
     while(no){
         TViz *viz = no->prim_viz;
         no->prim_viz = retirador_de_viz(viz, x);
         no = no->prox_no;
     }
+
 }
 
-// Retira um vertice especifico
+
+// Caso orientado
 
 void retira_aresta1(TG* g, int no, int x){
     TNO *p = busca_no(g,no);
@@ -182,8 +263,12 @@ void retira_aresta1(TG* g, int no, int x){
     }
 }
 
+// Caso nao orientado
+
 void retira_aresta0(TG* g, int no, int x){
+
     TNO *p = busca_no(g,no);
+
     if(p){
         p->prim_viz = retirador_de_viz(p->prim_viz, x);
     }
@@ -193,21 +278,26 @@ void retira_aresta0(TG* g, int no, int x){
     }
 }
 
-// Testa para saber se e orientado ou nao
+// Testa de uma aresta existe
 
 int testaAresta(TG *g, int no, int viz){
+
     TNO* p = g->prim_no;
+
     while((p)&&(p->id_no != no)){
         p = p->prox_no;
     }
+
     if(p == NULL)
         return 0;
-    
+
 
     TViz *l = p->prim_viz;
+
     while((l)&&(l->id_viz != viz)){
         l = l->prox;
     }
+
     if((l == NULL)){
         return 0;
     }
@@ -216,332 +306,283 @@ int testaAresta(TG *g, int no, int viz){
     }
 }
 
+// Testa se e orientado
+
 int eOrientado(TG *g){
+
     TNO *atual = g->prim_no;
+
     while(atual){
+
         TViz *viz_atual = atual->prim_viz;
+
         while(viz_atual){
+
             int aux = testaAresta(g, viz_atual->id_viz, atual->id_no);
-            if(aux == 1){
+            if(aux == 0)
                 return 1;
-            }
+            
             viz_atual = viz_atual->prox;
         }
+
         atual = atual->prox_no;
     }
     return 0;
 }
 
-void buscaConexoes(TG *g, int id, int *vetor, int tamanho, int marcador){
-    int ida, volta;
 
-    
-    for (int j = 0; j < (tamanho); j++)
-    {
-        
-        if(id != j){
-
-            ida = testaAresta(g, id+1, j+1);
-            volta = testaAresta(g, j+1, id+1);
-
-            if((ida || volta)&&(vetor[j] == 0)){
-                vetor[j] = marcador;
-                buscaConexoes(g, j, vetor,tamanho, marcador);
-            } 
-        }
-    
-    }
-
-}
-
-
+// Copia o grafo
 
 TG* copiaDados(TG* g, int tamanho){
+
     TG *copia = cria();
     int i;
     for(i = 1; i <= tamanho; i++){
-        insere_no(copia, i);
+        if(busca_no(g,i))
+            insere_no(copia, i);
     }
 
     TNO *p = g->prim_no;
     TNO *p2 = copia->prim_no;
-    while(p){       
+
+    while(p){
+
         TViz *viz = p->prim_viz;
         while(viz){
+
             insere_viz(copia,p2->id_no,viz->id_viz);
             viz = viz->prox;
+
         }
+
         p = p->prox_no;
-        p2 = p2->prox_no;       
+        p2 = p2->prox_no;
     }
 
     return copia;
 }
 
-void pontes(TG* g, int tamanho){
-    TG* copia = copiaDados(g,tamanho);
-    int* posicoes = (int*) malloc(tamanho * sizeof(int));
-    int contador =0, marcador=1,proximo, id_ponte;
 
-    TNO *p = copia->prim_no;
-    while(p){
-        TViz *vizSalvo = p->prim_viz;       
-        TViz *viz = p->prim_viz;
-        int contador=0;
-        while(viz){
-            contador++;
-            viz = viz->prox;
-        }
 
-        if(contador > 1)
-        {    
-            for (int i = 1; i <= contador; i++)
-            {
-
-                viz = p->prim_viz;
-                p->prim_viz=NULL;
-                for (int j = 1; j <= contador; j++)
-                {   
-                    if(viz)
-                    {
-                        
-                        if (j!=i)
-                        {
-                            TViz* v = (TViz*)malloc(sizeof(TViz));
-                            v->id_viz = viz->id_viz;
-                            v->prox = p->prim_viz;
-                            p->prim_viz = v;
-                        }
-                        else{
-                            id_ponte = viz->id_viz;
-                        }
-                        viz = viz->prox;
-                    }         
-                }
-
-                for (int k=0; k< tamanho; k++)
-                    posicoes[k]=0;
-
-                posicoes[0]=marcador;
-                buscaConexoes(copia,0,posicoes,tamanho, marcador);
-                for (int k=0; k< tamanho; k++)
-                    if(posicoes[k] == 0){
-                        proximo = k;
-                        marcador++;
-                        posicoes[k]=marcador;
-                        buscaConexoes(copia,k,posicoes,tamanho, marcador);
-                    }
-
-                if((marcador > 1)&&(p->id_no!=id_ponte)){
-                    printf("Ponte: %d %d\n", p->id_no,id_ponte);
-                    marcador = 1;
-                }    
+/*
+Baseado no capítulo 6.7 do livro Data Structures and Algorithms, De Aho, Hopcroft, Ullman,
+e no algoritmo de Tarjan.
+*/
+static void fortes( TG *g, int i, int *posicoes)
+{   
+    //criacao de variaveis
+    TNO  *no;
+    TViz *viz;
+    int min, id_viz;
  
-                liberaViz(p->prim_viz);
-                p->prim_viz = vizSalvo;
+    pre[i] = cont;
+    cont++;
+
+    min = pre[i];
+    no = busca_no(g,i+1);
+
+    // caso o no exista
+    if(no){
+
+        pilha[N] = no;
+        N++;
+        viz = no->prim_viz;
+
+        for(viz; viz != NULL; viz = viz->prox){
+
+            id_viz = viz->id_viz;
+
+            if (pre[id_viz - 1] == -1) {
+
+                fortes( g, id_viz-1, posicoes);
+                if (low[id_viz-1] < min) 
+                    min = low[id_viz-1];
+            }
+            else if ((pre[id_viz-1] < pre[i]) && (posicoes[id_viz-1] == -1)) {
+                if (pre[id_viz-1] < min) 
+                    min = pre[id_viz-1];
             }
         }
-        p = p->prox_no;
-    }
-    free(copia);
-}
 
-void buscaConexoes2(TG *g, int id, int *vetor, int tamanho, int marcador, int pular){
-    int ida, volta;
-    
-    for (int j = 0; j < (tamanho); j++)
-    {
-        
-        if((id != j)&&(j != pular)&&(id != pular)){
-            ida = testaAresta(g, id+1, j+1);
-            volta = testaAresta(g, j+1, id+1);
+        low[i] = min;
 
-            if((ida || volta)&&(vetor[j] == 0)){
-                vetor[j] = marcador;
-                buscaConexoes2(g, j, vetor,tamanho, marcador,pular);
+        if (low[i] == pre[i]) {
+
+            do{
+
+                --N;
+                no = pilha[N];
+                posicoes[no->id_no - 1] = k;
+
             } 
+            while (no->id_no - 1 != i);
+
+            k++;
         }
-    
     }
 
 }
 
-void pontoArticular(TG* g, int tamanho){
-    int* posicoes = (int*) malloc(tamanho * sizeof(int));
-    int contador =0, marcador=1,proximo, id_ponte;
 
-    for (int i = 1; i <= tamanho; i++)
-    {
-        TG* copia = copiaDados(g,tamanho);
-        retira(copia, i);
-
-        for (int k=0; k< tamanho; k++)
-            posicoes[k]=0;
-
-        posicoes[i-1]=marcador;
-        buscaConexoes2(copia,0,posicoes,tamanho, marcador,i-1);
-        for (int k=0; k< tamanho; k++)
-            if(posicoes[k] == 0){
-                proximo = k;
-                marcador++;
-                posicoes[k]=marcador;
-                buscaConexoes2(copia,k,posicoes,tamanho, marcador,i-1);
-            }
-        if(marcador > 1){
-            printf("Ponto de Articulacao: %d\n", i);
-            marcador = 1;
-        }
-
-        free(copia);
-    }
-    
-    
-}
-
-
-
-
-/*adaptado da figura 5.15 do livro de Aho, Hopcroft e Ullman
-https://www.ime.usp.br/~pf/algoritmos_para_grafos/aulas/tarjan.html*/
-static void strongR( TG *g, int v, int *sc) 
-{ 
-   TNO  *u; 
-   TViz *a; 
-   int min, w;
-
-   pre[v] = cnt++;
-   min = pre[v]; 
-   u = busca_no(g,v+1);
-   if(u){
-    stack[N++] = u;
-       a = u->prim_viz;
-       for (a ; a != NULL; a = a->prox) { 
-          w = a->id_viz;
-          if (pre[w -1] == -1) {
-             strongR( g, w-1, sc);
-             if (low[w-1] < min) min = low[w-1]; 
-          }
-          else if (pre[w-1] < pre[v] && sc[w-1] == -1) {
-             if (pre[w-1] < min) min = pre[w-1]; 
-          }
-       }
-       low[v] = min;
-       if (low[v] == pre[v]) {               
-          do {
-             u = stack[--N];
-             sc[u->id_no - 1] = k;
-          } while (u->id_no - 1 != v);
-          k++;
-       }
-   }
-   
-}
-
-int GRAPHscT( TG *g, int *sc, int tamanho)
+// Chama a busca em profundidade e o algoritomo de Tarjan
+int buscaP( TG *g, int *posicoes, int tamanho)
 {
-    int v; 
+    int i;
+
+    //alocacao das variaveis usadas
     pre = (int*) malloc( tamanho * sizeof (int));
     low = (int*) malloc( tamanho * sizeof (int));
-    stack = (TNO**) malloc( tamanho * sizeof (TNO));
+    pilha = (TNO**) malloc( tamanho * sizeof (TNO));
 
-    for (v = 0; v < tamanho; ++v) 
-      pre[v] = sc[v] = -1;
+    //inicia as variaveis
+    for (i = 0; i < tamanho; ++i)
+      pre[i] = posicoes[i] = -1;
 
-    k = N = cnt = 0;
-    for (v = 0; v < tamanho; ++v) 
-        if (pre[v] == -1)
-            strongR( g, v, sc);
+    k = N = cont = 0;
 
-    free(pre); free(low); free( stack);
+    // busca em profundidade + Algoritmo de Tarjan
+    for (i = 0; i < tamanho; ++i)
+        if (pre[i] == -1)
+            fortes( g, i, posicoes);  
+
+    //libera o que foi usado    
+    free(pre); 
+    free(low); 
+    free( pilha);
+
+    //retorna a quantidade de grafos fortemente conexos
     return k;
 }
 
 
+
+// Encontra as conexoes fortes
 void conexoesFortes(TG *g){
+
     TNO *atual = g->prim_no;
-    int contador =0, marcador=0, proximo, soma=0;
+    int contador =0, marcador=0, proximo, soma=0, j;
+
     while(atual){
+
         if(atual->id_no > contador)
             contador = atual->id_no;
+
         atual = atual->prox_no;
-        
+
     }
 
     if (contador < 2)
         return;
 
     int* posicoes = (int*) malloc(contador * sizeof(int));
-    
-    int al = GRAPHscT(g,posicoes, contador);
-   
-    for (int j = 0; j < contador; j++)
-    {   
+    int al = buscaP(g,posicoes, contador);
+
+    for(j = 0; j < contador; j++)
+    {
         if(posicoes[j] != -1)
         printf("Vertice: %d, Valor:%d\n", (j+1),posicoes[j]);
-        
+
     }
-    
+
     free(posicoes);
 }
 
-void ehConexo(TG *g){
+// Encontra os pontos de articulacao
+void pontoArticular(TG* g, int contador){
+
     TNO *atual = g->prim_no;
-    int contador =0, marcador=1,proximo;
-    while(atual){
-        if(atual->id_no > contador)
-            contador = atual->id_no;
-        atual = atual->prox_no;
-        
-    }
-
-    if (contador < 2)
-        return;
-
+    int quantidade, antes, i;
     int* posicoes = (int*) malloc(contador * sizeof(int));
 
-    for (int i=0; i< contador; i++)
-        posicoes[i]=0;
+    antes = buscaP(g,posicoes, contador);
 
-    
-    posicoes[0]=marcador;
-    buscaConexoes(g,0,posicoes,contador, marcador);
-    for (int i=0; i< contador; i++)
-        if(posicoes[i] == 0){
-            proximo = i;
-            marcador++;
-            posicoes[i]=marcador;
-            buscaConexoes(g,i,posicoes,contador, marcador);
-        }
-
-    for (int j = 0; j < marcador; j++)
+    for (i = 1; i <= contador; ++i)
     {
-        int quantidade=0;
-        for (int i=0; i< contador; i++)
-            if(posicoes[i] == j+1)
-            {    
-                quantidade++;
-            }
+        if(busca_no(g,i)){
 
-        //componentes conexas    
-        if(quantidade >= 2)
-        {    
-            printf("%s\n", "Componentes conexas:");
-            for (int i=0; i< contador; i++){
-                if(posicoes[i] == j+1)
-                {    
-                    printf("%d\n",i+1);
-                }    
-            }
-            printf("\n\n");
+            TG* copia = copiaDados(g,contador);
+            retira(copia,i);
+
+            quantidade = buscaP(copia,posicoes, contador);
+
+            if(quantidade>antes)
+                printf("Ponto de articulacao: %d\n", i);
+
+            free(copia);
         }
-        
     }
-    
-    free(posicoes);
 
-    pontes(g, contador);
-    pontoArticular(g, contador);
-   
+    free(posicoes);
+}
+
+
+// Encontra as pontes do grafo
+void pontes(TG* g, int contador){
+
+    TNO *atual = g->prim_no;
+    int quantidade, antes, i, j;
+    int* posicoes = (int*) malloc(contador * sizeof(int));
+
+    antes = buscaP(g,posicoes, contador);
+
+    for(i = 1; i <= contador; ++i)
+    {
+        
+        for(j = i+1; j<=contador; j++){
+
+            if((i!=j)&&(busca_no(g,i))&&(busca_no(g,j))&&(busca_aresta(g,i,j))){
+
+                TG* copia = copiaDados(g,contador);
+                retira_aresta1(copia, i, j);
+                retira_aresta1(copia, j, i);
+                quantidade = buscaP(copia,posicoes, contador);
+
+                if(quantidade>antes)
+                    printf("Ponte: %d %d\n", i, j);
+
+                free(copia);
+            }
+        }    
+    }
+
+    free(posicoes);
+}
+
+// Testa se e conexo, se for procura as pontes e pontos de articulacao
+void ehConexo(TG *g){
+
+    TNO *atual = g->prim_no;
+    int tamanho =0, marcador=0, proximo, soma=0, j;
+
+    while(atual){
+        if(atual->id_no > tamanho)
+            tamanho = atual->id_no;
+        atual = atual->prox_no;
+    }
+
+    if(tamanho < 2)
+        return;
+
+    int* posicoes = (int*) malloc(tamanho * sizeof(int));
+
+    int al = buscaP(g,posicoes, tamanho);
+
+    if(al > 1){
+        printf("O grafo nao eh conexo.\n");
+        for(j = 0; j < tamanho; j++)
+        {
+            if(posicoes[j] != -1)
+                printf("Vertice: %d, Valor:%d\n", (j+1),posicoes[j]);
+
+        }
+
+    }
+    else{
+        pontes(g, tamanho);
+        pontoArticular(g, tamanho);
+    }
+
+    free(posicoes); 
 
 }
 
--- Grafos-ED1/main.c
+++ Grafos-ED1/main.c
@@ -1,20 +1,24 @@
+/*
+    Alunos: Diego Carrico Cacau
+            Vitor Hardoim
+*/
+
 #include "grafo.h"
 
 int main(){
     // Abertura do arquivo
     FILE *file;
     file = fopen("grafo.txt", "r");
-    // Salvado o tamanho e se o grafo é orientado ou nao
-    int tam, orientacao;
+    // Salvado o tamanho
+    int tam;
     fscanf(file, "%d", &tam);
-    // Criando e 0preenchendo o grafo, testando se e orientado ou nao
+    // Criando e preenchendo o grafo, testando se e orientado ou nao
     TG *grafo = cria();
-    //orientacao = eOrientado(grafo);
     int i;
     for(i = 1; i <= tam; i++){
         insere_no(grafo, i);
     }
-    
+
     while(!feof(file)){
         int prim, sec;
         fscanf(file, "%d %d", &prim, &sec);
@@ -23,26 +27,25 @@ int main(){
     // Fechando o arquivo
     fclose(file);
 
-
     //##########MENU##########
     int menu = 1, op, no, aresta;
     TNO *p, *l;
     TViz *t;
-    
+
     while(menu == 1){
-        printf("\n##########MENU##########\n" \
-               "#1 para inserir um no\n" \
-               "#2 para imprimir o grafo\n" \
-               "#3 para sair\n" \
-               "#4 para inserir uma aresta\n" \
-               "#5 para retirar um no inteiro\n" \
-               "#6 para verificar se o grafo e orientado\n" \
-               "#7 para retirar uma aresta especifica\n" \
-               "#8 para buscar um no\n" \
-               "#9 para buscar uma aresta\n" \
-               "#10 para executar a funcao completa do trabalho\n" \
-               "########################\n\n");
-        
+        printf("\n################## MENU ###################\n" \
+               "# 1 para inserir um no                    #\n" \
+               "# 2 para inserir um vizinho em um no      #\n" \
+               "# 3 para retirar um no inteiro            #\n" \
+               "# 4 para retirar uma aresta especifica    #\n" \
+               "# 5 para buscar um no                     #\n" \
+               "# 6 para buscar uma aresta                #\n" \
+               "# 7 para verificar se o grafo e orientado #\n" \
+               "# 8 para resposta completa                #\n" \
+               "# 9 para imprimir o grafo                 #\n" \
+               "# 10 para sair                            #\n" \
+               "###########################################\n\n");
+
         printf("Operacao: ");
         scanf("%d",&op);
         switch(op){
@@ -52,50 +55,43 @@ int main(){
                 insere_no(grafo, no);
                 break;
             case 2:
-                imprime(grafo);
-                break;
-            case 3:
-                menu = -1;
-                break;
-            case 4:
-                printf("No que deseja inserir: ");
+                printf("No em que deseja inserir: ");
                 scanf("%d",&no);
-                printf("\nVizinho: ");
+                printf("Vizinho: ");
                 scanf("%d", &aresta);
 
                 p = busca_no(grafo, no);
                 l = busca_no(grafo, aresta);
                 if((p)&&(l)){
-                    insere_viz( grafo, no, aresta);
-                }else{
+                    if(eOrientado(grafo)){
+                        insere_viz(grafo, no, aresta);
+                    }else{
+                        insere_viz(grafo, no, aresta);
+                        insere_viz(grafo, aresta, no);
+                    }
+                }
+                else{
                     printf("Nao foi possivel inserir o vizinho\n");
                 }
 
                 break;
-            case 5:
+            case 3:
                 printf("No que deseja retirar: ");
                 scanf("%d",&no);
                 retira(grafo, no);
                 break;
-            case 6:
-                if(eOrientado(grafo)){
-                    printf("O grafo e orientado\n");
-                }else{
-                    printf("O grafo nao e orientado\n");
-                }
-                break;
-            case 7:
+            case 4:
                 printf("No em que deseja retirar: ");
                 scanf("%d",&no);
                 printf("\nVizinho a ser retirado: ");
                 scanf("%d", &aresta);
                 if(eOrientado(grafo)){
-                    retira_aresta0(grafo, no, aresta);
-                }else{
                     retira_aresta1(grafo, no, aresta);
+                }else{
+                    retira_aresta0(grafo, no, aresta);
                 }
                 break;
-            case 8:
+            case 5:
                 printf("No: ");
                 scanf("%d", &no);
                 p = busca_no(grafo, no);
@@ -105,7 +101,7 @@ int main(){
                     printf("O no %d nao existe\n", no);
                 }
                 break;
-            case 9:
+            case 6:
                 printf("No: ");
                 scanf("%d", &no);
                 printf("Aresta: ");
@@ -117,24 +113,30 @@ int main(){
                     printf("A aresta %d do no %d nao existe\n", aresta, no);
                 }
                 break;
-            case 10:
+            case 7:
                 if(eOrientado(grafo)){
-                    printf("O grafo e orientado\n"); 
+                    printf("O grafo e orientado\n");
+                }else{
+                    printf("O grafo nao e orientado\n");
+                }
+                break;
+            case 8:
+                if(eOrientado(grafo)){
+                    printf("O grafo e orientado\n");
                     conexoesFortes(grafo);
                 }else{
                     printf("O grafo nao e orientado\n");
                     ehConexo(grafo);
                 }
                 break;
-
+            case 9:
+                imprime(grafo);
+                break;
+            case 10:
+                menu = -1;
+                liberaGrafo(grafo);
+                break;
         }
     }
-    //##########MENU##########
-
-    free(p);
-    free(l);
-    free(t);
-
-    //imprime(grafo);
     return -1;
 }
